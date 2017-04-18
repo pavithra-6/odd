e9945ee8c124f6be0eb250a179878da0a8b81595
pavithra-6 <paviikannan1996@gmail.com>
2017-04-18 15:12:42 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int y;
+printf("enter the year:");
+scanf("%d",&y);
+if(y%4==0)
+   {
+printf("even year");
+   }
+   else
+    {
+    printf("odd year");
+    }
+   return 0;
+   }
