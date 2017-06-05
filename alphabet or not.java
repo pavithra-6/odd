fb0a576e3c73438127fd884de8d191b9b440d1d8
pavithra-6 <paviikannan1996@gmail.com>
2017-06-05 15:39:22 +0530

    Update alphabet or not.java

--- alphabet or not.java
+++ alphabet or not.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class java
+public class alphaet
 {
 public static void main(String args[])
 {
@@ -7,13 +7,13 @@ char ch;
 Scanner n=new Scanner(System.in);
 ch=n.next().charAt(0);
 System.out.println("enter the characters:");
-if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+if((ch>="a")||(char<="z"))
 {
-System.out.println("this is vowels");
+System.out.println("this is alphabet");
 }
 else
 {
-System.out.println("this is not a vowels");
+System.out.println("this is not an alphabet");
 }
 }
 }
