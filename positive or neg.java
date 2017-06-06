a5ed3e81d3685490e78ffd41c29955f39ba3f820
pavithra-6 <paviikannan1996@gmail.com>
2017-06-06 13:42:35 +0530

    Create positive or neg.java

---
+++ positive or neg.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class pos
+{
+public static void main(String args[])
+{
+int n;
+Scanner a=new Scanner(Ststem.in);
+System.out.println("enter the number:");
+if(n>0)
+{
+System.out.println("the no is positive:"+n);
+}
+else if(n<0)
+{
+System.out.println("the no is negative:"+n);
+}
+else
+{
+System.out.println("the no is enither positive nor negative:"+n);
+}
+}
+}
