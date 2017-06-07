d69e11bb988e7c4b95299afc6fa05cce121258f9
pavithra-6 <paviikannan1996@gmail.com>
2017-06-07 10:00:42 +0530

    Create factotial.java

---
+++ factotial.java
@@ -0,0 +1,14 @@
+class ha
+{
+public static viod main(String args[])
+{
+int i,fact=1;
+int num=5;
+for(i=1;;i<=5;i++)
+{
+fact=fact+i;
+}
+System.out.println("factorial of given num is:"+fact);
+}
+}
+
