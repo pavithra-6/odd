c1ff451152606b9150937d589e14f1fdd0704ca2
pavithra-6 <paviikannan1996@gmail.com>
2017-06-30 11:31:42 +0530

    Add files via upload

---
+++ Oddbt2intervel.java
@@ -0,0 +1,15 @@
+package sumave1;
+
+public class Oddbt2intervel {
+
+	public static void main(String[] args) {
+	int i=50;
+	for(int j=1;j<50;j++){
+		if(j%2!=0){
+			System.out.println(j);
+		}
+	}
+		
+	}
+
+}
