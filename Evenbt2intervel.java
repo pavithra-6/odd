98cdc6ddceb7410d42c7fb6dcfc35747c5a76380
pavithra-6 <paviikannan1996@gmail.com>
2017-06-30 11:34:07 +0530

    Add files via upload

---
+++ Evenbt2intervel.java
@@ -0,0 +1,15 @@
+package sumave1;
+
+public class Evenbt2intervel {
+
+	public static void main(String[] args) {
+		int c=10;
+		for(int v=0;v<10;v++){
+			if(v%2==0){
+				System.out.println(v);
+			}
+		}
+
+	}
+
+}
