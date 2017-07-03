bf88358900c2d8b89623348ff8edd989ce0d992d
pavithra-6 <paviikannan1996@gmail.com>
2017-07-03 17:21:17 +0530

    Add files via upload

---
+++ Decimaltobinary.java
@@ -0,0 +1,11 @@
+package sumave1;
+
+public class Decimaltobinary {
+
+	public static void main(String[] args) {
+		System.out.println(124);
+		System.out.println(Integer.toBinaryString(124));
+
+	}
+
+}
