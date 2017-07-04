f33cd5e340c67712c66a9964a7b4b949a5167fee
pavithra-6 <paviikannan1996@gmail.com>
2017-07-04 09:32:56 +0530

    Add files via upload

---
+++ Revstring.java
@@ -0,0 +1,20 @@
+package sumave1;
+
+import java.util.Scanner;
+
+public class Revstring {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String rev="";
+		String s;
+		System.out.println("enter the string s1");
+		Scanner c=new Scanner(System.in);
+		String s1=c.nextLine();
+		int l=s1.length();
+for(int i=s1.length()-1;i>=0;i--)
+{
+	rev=rev+s1.charAt(i);
+}
+System.out.println("reverse string is:"+rev);
+	}
+}
